72dd1b6e173f83423e8b5ad425077900ce15e06e
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-02-16 09:43:01 +0000

    clean up code and added comments

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 var appliances = [];
-    var ratePerUnit = 0.10;
-    function addAppliance() {
-      var selectedAppliance = document.getElementById("appliance");
-      var applianceName = selectedAppliance.options[selectedAppliance.selectedIndex].text;
-      var applianceWattage = selectedAppliance.value;
-      var hoursUsed = document.getElementById("hours").value;
-      var unitsConsumed = (applianceWattage / 1000) * hoursUsed;
-      var billAmount = ratePerUnit * unitsConsumed;
-      var appliance = {
-        name: applianceName,
-        wattage: applianceWattage,
-        hours: hoursUsed,
-        units: unitsConsumed,
-        bill: billAmount
-      };
-      appliances.push(appliance);
-      updateApplianceList();
-      updateTotalBill();
-    }
-    function updateApplianceList() {
-      var list = document.getElementById("appliance-list");
-      list.innerHTML = "";
-      for (var i = 0; i < appliances.length; i++) {
-      var appliance = appliances[i];
-      var listItem = document.createElement("li");
-      listItem.innerHTML = appliance.name + " (" + appliance.wattage + "W, " + appliance.hours + " hours per day) - " + appliance.bill.toFixed(2) + " USD";
-      list.appendChild(listItem);
-      }
-    }
+var ratePerUnit = 0.10;
+
+//function to add chosen appliance to the list
+function addAppliance() {
+  var selectedAppliance = document.getElementById("appliance");
+  var applianceName = selectedAppliance.options[selectedAppliance.selectedIndex].text;
+  var applianceWattage = selectedAppliance.value;
+  var hoursUsed = document.getElementById("hours").value;
+  var unitsConsumed = (applianceWattage / 1000) * hoursUsed;
+  var billAmount = ratePerUnit * unitsConsumed;
+  var appliance = {
+    name: applianceName,
+    wattage: applianceWattage,
+    hours: hoursUsed,
+    units: unitsConsumed,
+    bill: billAmount
+  };
+  appliances.push(appliance);
+  updateApplianceList();
+  //updateTotalBill();
+}
+
+//function to display/update the items in the list
+function updateApplianceList() {
+  var list = document.getElementById("appliance-list");
+  list.innerHTML = "" //refresh the list
+
+  appliances.forEach(item => {
+    list.innerHTML += `
+    <li>${item.name} (${item.wattage}W, ${item.hours} hours per day) - ${item.bill.toFixed(2)} USD </li>
+    `
+  })
+  console.log(appliances)
+}
